e35187ce3ed9733dda2d375725a13bd12ba2bc85
Nelson Nunes <nelson.nunes@eoh.com>
2025-05-20 13:52:32 +0200

    Enhance API service registration and database connection Updated ApiServiceCollectionExtensions to include new services for infrastructure, persistence, and match simulation. Changed class name for clarity. Modified Forge.Api.csproj to add new package and project references for Entity Framework Core and match simulation. Updated appsettings.json to include a connection string for SQLite database.

--- src/Api/Forge.Api/Extensions/ApiServiceCollectionExtensions.cs
+++ src/Api/Forge.Api/Extensions/ApiServiceCollectionExtensions.cs
@@ -1,15 +1,17 @@
 ﻿using FastEndpoints;
 using FastEndpoints.Swagger;
-using Forge.Core;
 using Forge.Core.Extensions;
-using Forge.Infrastructure;
+using Forge.Infrastructure.Extensions;
+using Forge.MatchSimulation;
+using Forge.Persistence.Extensions;
+using Forge.Persistence.Sqlite.Extensions;
 
 namespace Forge.Api.Extensions;
 
 /// <summary>
 /// Extension methods for registering the Forge API services.
 /// </summary>
-public static class ServiceCollectionExtensions
+public static class ApiServiceCollectionExtensions
 {
     /// <summary>
     /// Registers the Forge API services with the specified configuration.
@@ -20,7 +22,13 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddForgeApi(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddForgeCore(configuration);
+        
         services.AddForgeInfrastructure();
+        
+        services.AddForgePersistence(configuration)
+                .AddForgeSqlitePersistence(configuration);
+
+        services.AddForgeMatchSimulation(configuration);
 
         // Register all modules
         services.RegisterModules(configuration);
